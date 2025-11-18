7e7db7a3a733f664c0d39db05e1df27c01a35c32
Михаэль Зайдман <mihaelzajdman@MacBook-Air-Mihael.local>
2025-11-18 23:19:35 +0300

    Remove embedded repo and add as regular directory

---
+++ convex-helpers-src/eslint.config.mjs
@@ -0,0 +1,85 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import convexPlugin from "@convex-dev/eslint-plugin";
+
+export default [
+  { files: ["src/**/*.{js,mjs,cjs,ts,tsx}", "convex/**/*.{ts,tsx}"] },
+  {
+    ignores: [
+      "dist/**",
+      "packages/convex-helpers/dist/**",
+      "packages/convex-helpers/generate-exports.mjs",
+      "src/fakeConvexClient/fakeConvexClient.js",
+      "backendHarness.js",
+      "eslint.config.mjs",
+      "convex/vitest.config.mts",
+      "setup.cjs",
+      "**/_generated/",
+      "vite.config.mts",
+      "vitest.workspace.ts",
+    ],
+  },
+  {
+    files: ["convex/**/*.ts", "packages/convex-helpers/server/**/*.ts"],
+    ignores: ["packages/convex-helpers/server/_generated/**/*"],
+    plugins: {
+      "@convex-dev": convexPlugin,
+    },
+    rules: convexPlugin.configs.recommended[0].rules,
+  },
+  {
+    languageOptions: {
+      globals: globals.worker,
+      parser: tseslint.parser,
+
+      parserOptions: {
+        project: ["./tsconfig.json", "./packages/convex-helpers/tsconfig.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: [
+      "src/react/**/*.{jsx,tsx}",
+      "src/react/**/*.js",
+      "src/react/**/*.ts",
+    ],
+    plugins: { react: reactPlugin, "react-hooks": reactHooks },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...reactPlugin.configs["recommended"].rules,
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+      "eslint-comments/no-unused-disable": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // allow (_arg: number) => {} and const _foo = 1;
+      "no-unused-vars": "off",
+      "no-unused-private-class-members": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
---
+++ convex-helpers-src/publish.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+
+set -e
+
+rm -rf packages/convex-helpers/node_modules
+npm i
+npm run clean
+npm run build
+npm i
+npm run lint
+npm run test
+git diff --exit-code || {
+  echo "Uncommitted changes found. Commit or stash them before publishing."
+  exit 1
+}
+function cleanup() {
+  git checkout -- :/packages/convex-helpers/package.json
+}
+trap cleanup EXIT
+
+pushd packages/convex-helpers >/dev/null
+if [ "$1" == "alpha" ]; then
+  npm version prerelease --preid alpha
+else
+  npm version patch
+fi
+current=$(npm pkg get version | tr -d '"')
+popd >/dev/null
+
+cat <<EOF
+Test it:
+  - Add some example usage to the outer convex-helpers repo.
+  - Run \`npm pack\` in /dist and install it elsewhere from the .tgz file.
+EOF
+echo "Latest versions:"
+npm view convex-helpers@latest version
+npm view convex-helpers@alpha version
+
+read -r -p "Enter the new version number (hit enter for $current): " version
+
+pushd packages/convex-helpers >/dev/null
+if [ -n "$version" ]; then
+  npm pkg set version="$version"
+else
+  version=$current
+fi
+
+cp package.json dist/
+
+cd dist
+if (echo "$version" | grep alpha >/dev/null); then
+  npm publish --tag alpha --dry-run
+else
+  npm publish --dry-run
+fi
+popd >/dev/null
+echo "^^^ DRY RUN ^^^"
+read -r -p "Publish $version to npm? (y/n): " publish
+if [ "$publish" = "y" ]; then
+  npm i
+  pushd packages/convex-helpers/dist >/dev/null
+  if (echo "$version" | grep alpha >/dev/null); then
+    npm publish --tag alpha
+  else
+    npm publish
+  fi
+  popd >/dev/null
+  git add package.json package-lock.json packages/convex-helpers/package.json
+  # If there's nothing to commit, continue
+  git commit -m "npm $version" || true
+  git tag "npm/$version"
+  git push origin "npm/$version"
+  git push
+else
+  echo "Aborted."
+fi
